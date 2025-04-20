deae35c6f16d86e8e867d9230ad557efcf81d739
Sergey Luzik <luziksergey@gmail.com>
2025-04-20 14:58:13 +0300

    feat: adds classname

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,6 @@ import { App } from "./App";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App className="p-10 pt-5" />
+    <App className="pt-2 p-10 pt-5" />
   </StrictMode>
 );
