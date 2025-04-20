e715097b4b3f8425dbc1e290dc999590f115dbee
Sergey Luzik <luziksergey@gmail.com>
2025-04-20 14:48:19 +0300

    ref: changes App classnames

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { twMerge } from "tailwind-merge";
 
 export const App = ({ className }) => {
-  const baseClasses = "p-20 bg-blue-500";
+  const baseClasses = "p-30 bg-blue-500 pt-6";
   const mergedClasses = twMerge(baseClasses, className); // className = "p-10"
   return <div className={mergedClasses}>...</div>;
 };
--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,6 @@ import { App } from "./App";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App className="pt-5" />
+    <App className="p-10 pt-5" />
   </StrictMode>
 );
