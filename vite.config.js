a49e1dac026d2a7bc5b1d17216310aea70b3183e
Sergey Luzik <luziksergey@gmail.com>
2025-04-20 13:51:41 +0300

    ref: renames project

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/twMerge/'
+  base: "/react-tailwind-merge-test/",
 });
